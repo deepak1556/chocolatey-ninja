ae75a1b51cefa275c046a6608d0abe985a3dd13f
deepak1556 <hop2deep@gmail.com>
2015-02-25 16:15:15 +0530

    update ninja version

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@ try {
   $sysDrive = $env:SystemDrive
   $ninjaPath =  "$sysDrive/tools/ninja"
 
-  Install-ChocolateyZipPackage 'ninja' 'https://github.com/martine/ninja/releases/download/v1.5.1/ninja-win.zip' $ninjaPath
+  Install-ChocolateyZipPackage 'ninja' 'https://github.com/martine/ninja/releases/download/v1.5.3/ninja-win.zip' $ninjaPath
   Install-ChocolateyPath $ninjaPath
 
   write-host 'ninja has been installed. Call ninja -h from the command line to see the list of options. You may need to close and repoen the command shell.'
