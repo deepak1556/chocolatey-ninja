54ab562fba778707e875efc21a5aa9b57eca5326
deepak1556 <hop2deep@gmail.com>
2016-06-07 14:13:24 +0530

    bump version v1.7.1

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,4 +1,4 @@
 $packageName = 'ninja'
-$url = 'https://github.com/martine/ninja/releases/download/v1.6.0/ninja-win.zip'
+$url = 'https://github.com/ninja-build/ninja/releases/download/v1.7.1/ninja-win.zip'
 
 Install-ChocolateyZipPackage $packagename $url $(Split-Path -parent $MyInvocation.MyCommand.Definition)
